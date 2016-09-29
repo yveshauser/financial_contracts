a2d0d12ecb931885efe1c39f5048ec32f1f3fece
yves <yves.hauser@gmail.com>
2016-09-29 16:09:49 -0400

    formatting

--- Valuation.hs
+++ Valuation.hs
@@ -19,9 +19,9 @@ type Process m a = m (Trace a)
 data Model m  where
   Model :: MonadDist m => {
     modelStart :: Date
-      , exch :: Currency -> Currency -> Process m Double
-      , disc :: Currency -> (Process m Bool, Process m Double) -> Process m Double
-      , snell :: Currency -> (Process m Bool, Process m Double) -> Process m Double
+      , exch   :: Currency -> Currency -> Process m Double
+      , disc   :: Currency -> (Process m Bool, Process m Double) -> Process m Double
+      , snell  :: Currency -> (Process m Bool, Process m Double) -> Process m Double
       , absorb :: Currency -> (Process m Bool, Process m Double) -> Process m Double
 } -> Model m
 
@@ -39,35 +39,35 @@ evalC m@(Model modelDate exch disc snell absorb) k = eval
         eval (Anytime o c)  = snell k (evalO m k o, eval c)
         eval (Until o c)    = absorb k (evalO m k o, eval c)
 
-bigK :: MonadDist m => a -> Process m a
-bigK t = fmap const $ categorical [(t, 1.0)]
-
 evalO :: MonadDist m => Model m -> Currency -> Obs a -> Process m a
-evalO m k (Konst a) = bigK a
-evalO m k (Lift f a) = (.) <$> return f <*> evalO m k a
+evalO m k (Konst a)     = bigK a
+evalO m k (Lift f a)    = (.) <$> return f <*> evalO m k a
 evalO m k (Lift2 f a b) = liftA2 f <$> evalO m k a <*> evalO m k b
-evalO m k (Date) = return id
-evalO m k (Value c) = evalC m k c
+evalO m k (Date)        = return id
+evalO m k (Value c)     = evalC m k c
 
 instance Num a => Num (Trace a) where
   fromInteger i t = fromInteger i
-  (+) a b t = a t + b t
-  (-) a b t = a t - b t
-  (*) a b t = a t * b t
-  abs a t = abs (a t)
-  signum a t = signum (a t)
-  negate a t = negate (a t)
+  (+) a b t       = a t + b t
+  (-) a b t       = a t - b t
+  (*) a b t       = a t * b t
+  abs a t         = abs (a t)
+  signum a t      = signum (a t)
+  negate a t      = negate (a t)
+
+instance (Num a, MonadDist m) => Num (Process m a) where
+  fromInteger i = bigK (fromInteger i)
+  (+)           = liftA2 (+)
+  (-)           = liftA2 (-)
+  (*)           = liftA2 (*)
+  abs           = fmap abs
+  signum        = fmap signum
+
+bigK :: MonadDist m => a -> Process m a
+bigK t = fmap const $ categorical [(t, 1.0)]
 
 max' :: Ord a => Trace a -> Trace a -> Trace a
 max' a b t = max (a t) (b t)
 
 cond' :: Trace Bool -> Trace Double -> Trace Double -> Trace Double
 cond' bs xs ys t = if (bs t) then (xs t) else (ys t)
-
-instance (Num a, MonadDist m) => Num (Process m a) where
-  fromInteger i = bigK (fromInteger i)
-  (+) = liftA2 (+)
-  (-) = liftA2 (-)
-  (*) = liftA2 (*)
-  abs = fmap abs
-  signum = fmap signum
