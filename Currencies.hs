bb1216363b77ca76ea501e8a6106fb9ff9bb6d2a
yves <yves.hauser@gmail.com>
2017-04-08 12:33:18 -0400

    time, json instances in module

--- Currencies.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE DeriveGeneric #-}
-
-module Currencies where
-
-import GHC.Generics -- TODO: currently used for JSON instances - do these manually instead
-
-data Currency = CHF | EUR | USD deriving (Ord, Eq, Show, Generic)
-
