73cea9ca697d4da7ee19efb917405c5c7db48adb
Yves Hauser <yves.hauser@gmail.com>
2022-05-30 21:42:57 +0200

    formatting

--- src/Valuation.hs
+++ src/Valuation.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE GADTs, RankNTypes, TypeSynonymInstances, FlexibleInstances, LiberalTypeSynonyms, IncoherentInstances #-}
 module Valuation where
 
-import Prelude hiding (and, or)
+import Prelude hiding (and, or, zipWith)
 import Control.Applicative (liftA2, liftA3)
 
 import Contracts
@@ -9,8 +9,25 @@ import Contracts
 -- trace
 type Trace a = Time -> a
 
+instance Num a => Num (Trace a) where
+  fromInteger = const . fromInteger
+  (+) a b t   = a t + b t
+  (-) a b t   = a t - b t
+  (*) a b t   = a t * b t
+  abs a t     = abs (a t)
+  signum a t  = signum (a t)
+  negate a t  = negate (a t)
+
 -- value process
 type Process m a = m (Trace a)
+
+instance (Num a, Applicative m) => Num (Process m a) where
+  fromInteger = bigK . fromInteger
+  (+)         = liftA2 (+)
+  (-)         = liftA2 (-)
+  (*)         = liftA2 (*)
+  abs         = fmap abs
+  signum      = fmap signum
 
 -- model
 data Model m where
@@ -25,16 +42,17 @@ data Model m where
 -- evaluation of contracts
 evalC :: Applicative m => Model m -> Currency -> Contract -> Process m Double
 evalC m@(Model _ exch disc snell absorb) k = eval
-  where eval Zero                = bigK 0
-        eval (One k1)            = exch k k1
-        eval (Give c)            = -(eval c)
-        eval (o `Scale` c)       = evalO m k o * eval c
-        eval (c1 `And` c2)       = eval c1 + eval c2
-        eval (c1 `Or` c2)        = liftA2 max' (eval c1) (eval c2)
-        eval (Cond o c1 c2)      = liftA3 cond' (evalO m k o) (eval c1) (eval c2)
-        eval (When o c)          = disc k (evalO m k o, eval c)
-        eval (Anytime o c)       = snell k (evalO m k o, eval c)
-        eval (Until o c)         = absorb k (evalO m k o, eval c)
+  where
+    eval Zero           = bigK 0
+    eval (One k1)       = exch k k1
+    eval (Give c)       = -(eval c)
+    eval (o `Scale` c)  = evalO m k o * eval c
+    eval (c1 `And` c2)  = eval c1 + eval c2
+    eval (c1 `Or` c2)   = liftA2 max' (eval c1) (eval c2)
+    eval (Cond o c1 c2) = liftA3 cond' (evalO m k o) (eval c1) (eval c2)
+    eval (When o c)     = disc k (evalO m k o, eval c)
+    eval (Anytime o c)  = snell k (evalO m k o, eval c)
+    eval (Until o c)    = absorb k (evalO m k o, eval c)
 
 evalO :: Applicative m => Model m -> Currency -> Obs a -> Process m a
 evalO m k (Konst a)     = bigK a
@@ -45,28 +63,14 @@ evalO m k (Before t)    = pure (t<)
 evalO m k (After t)     = pure (t>)
 evalO m k (Value c)     = evalC m k c
 
-instance Num a => Num (Trace a) where
-  fromInteger = const . fromInteger
-  (+) a b t   = a t + b t
-  (-) a b t   = a t - b t
-  (*) a b t   = a t * b t
-  abs a t     = abs (a t)
-  signum a t  = signum (a t)
-  negate a t  = negate (a t)
-
-instance (Num a, Applicative m) => Num (Process m a) where
-  fromInteger = bigK . fromInteger
-  (+)         = liftA2 (+)
-  (-)         = liftA2 (-)
-  (*)         = liftA2 (*)
-  abs         = fmap abs
-  signum      = fmap signum
-
 bigK :: Applicative m => a -> Process m a
 bigK = pure . const
 
+zipWith :: Ord a => (a -> a -> a) -> Trace a -> Trace a -> Trace a
+zipWith f a b t = f (a t) (b t)
+
 max' :: Ord a => Trace a -> Trace a -> Trace a
-max' a b t = max (a t) (b t)
+max' = zipWith max
 
 cond' :: Trace Bool -> Trace Double -> Trace Double -> Trace Double
 cond' bs xs ys t = if bs t then xs t else ys t
